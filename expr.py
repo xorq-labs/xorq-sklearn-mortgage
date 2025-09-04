a9b379bcee1aa5abfa1090721e60ce8139fa8e22
Hussain Sultan <hussainz@gmail.com>
2025-09-04 11:16:02 -0400

    add timing for quickgrove rewriting

--- expr.py
+++ expr.py
@@ -1,12 +1,13 @@
+import xorq.api as xo
 from attrs import evolve
 from codetiming import Timer
 
 from xorq_sklearn_mortgage.pipeline_lib import (
+    MLPipelineResult,
     ModelConfig,
     PipelineConfig,
-    MLPipelineResult,
 )
-
+from xorq_sklearn_mortgage.rewrite import rewrite_quickgrove_expr
 
 config = PipelineConfig(
     model=evolve(ModelConfig(), num_boost_round=50, max_depth=12),
@@ -19,3 +20,11 @@ with Timer("generate predict_quickgrove expr"):
     expr = new_predictions_quickgrove = result.predict_quickgrove(result.raw_expr)
 with Timer("execute predictions"):
     df = expr.limit(200_000).execute()
+with Timer("add filter to expr"):
+    filtered_expr = expr.filter(xo._.orig_rate<6)
+with Timer("execute predictions (orig_rate<6)"):
+    filtered_df = filtered_expr.execute()
+with Timer("rewrite quickgrove expr"):
+    rewritten_filtered_expr = rewrite_quickgrove_expr(filtered_expr)
+with Timer("execute predictions (orig_rate<6 with rewrite)"):
+    rewritten_filtered_df = rewritten_filtered_expr.execute()
